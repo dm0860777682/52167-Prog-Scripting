bf7335b76c4630c0caea131422f05007fc53354e
DónalM <dm0860777682@gmail.com>
2020-02-04 12:32:14 +0000

    Add files via upload

---
+++ Week3Exercise.py
@@ -0,0 +1,13 @@
+print("Loop over String program ")
+# Write a program that takes askes a user to input a string  and outputs every seconnd letter in reverse order.
+try:
+    #Ask user for input
+    UserSentence = input("Enter your Sentence: ")
+    senlength = len(UserSentence)
+    #Iterate over the user input string and print out every 2nd element
+    #### string[start:stop:step size]####
+    for x in UserSentence[0:senlength:2]:
+        print(x)
+except Exception:
+    #This will stop program will not fall over if this user does not enter a integer. 
+    print("sorry, somthing went wrong!")
